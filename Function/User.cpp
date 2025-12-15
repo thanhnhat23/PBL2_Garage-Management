949342ad4ab63e8e6fa2c5eeaa2989bb42c3297c
vntruong16 <nvttruong2006@gmail.com>
2025-12-15 23:28:14 +0700

    Route: • FIX nhập thêm route ấn refresh sẽ không xuất hiện • FIX nhập không đầy đủ thì vẫn lưu bình thường
    Ticket:
    • FIX: Chỉ được đặt vé trong tương lai
    • UPDATE: Nên đặt vé bằng ROUTE trước rồi mới chọn TRIP
    
    User
    • Update: Khi đăng ký TK phải nhập sđt
    • Update: Có thể xem vé nằm ở mục vé của tôi khi vé được đặt bởi admin bằng sđt
    • Update: Khi đặt vé phải nhập cả họ và tên, sđt sẽ được nhập sẵn có thể đổi

--- Function/User.cpp
+++ Function/User.cpp
@@ -1,11 +1,12 @@
 #include "../Class/User.h"
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
-User::User() : role(Role::USER) {}
+User::User() : role(Role::USER), phoneNumber("") {}
 
-User::User(std::string id, std::string uname, std::string pwd, Role r)
-: userId(id), username(uname), password(pwd), role(r) {}
+User::User(std::string id, std::string uname, std::string pwd, Role r, std::string phone)
+: userId(id), username(uname), password(pwd), role(r), phoneNumber(phone) {}
 
 void User::display() const {
     using namespace std;
@@ -13,19 +14,27 @@ void User::display() const {
          << setw(8)  << userId
          << setw(16) << username
          << setw(8)  << (role == Role::ADMIN ? "ADMIN" : "USER")
+         << setw(15) << phoneNumber 
          << "\n";
 }
 
 std::string User::toCSV() const {
-    return userId + "," + username + "," + password + "," + RoleToString(role);
+    return userId + "," + username + "," + password + "," + RoleToString(role) + "," + phoneNumber;
 }
 
 User User::fromCSV(const std::string& line) {
     std::stringstream ss(line);
-    std::string id, uname, pwd, r;
+    std::string id, uname, pwd, r, phone;
+    
     if (!std::getline(ss, id, ',')) return User();
     if (!std::getline(ss, uname, ',')) return User();
     if (!std::getline(ss, pwd, ',')) return User();
     if (!std::getline(ss, r, ',')) return User();
-    return User(id, uname, pwd, RoleFromString(r));
+    if (std::getline(ss, phone, ',')) {
+        if (!phone.empty() && phone.back() == '\r') phone.pop_back();
+    } else {
+        phone = "";
+    }
+
+    return User(id, uname, pwd, RoleFromString(r), phone);
 }
